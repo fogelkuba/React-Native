dc5c9f85c13ce3538f07f219e6f8191be21a3057
fogelkuba <fogelkuba@gmail.com>
2017-12-20 00:50:15 +0100

    initial

---
+++ albums/index.js
@@ -0,0 +1,15 @@
+//Import library
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src//components/Header';
+import AlbumList from './src/components/AlbumList'
+
+//Create Componenet
+const App = () =>(
+    <View>
+        <Header headerText={'Albums'} />
+        <AlbumList/>
+    </View>
+)
+//Render to device
+AppRegistry.registerComponent('albums', () => App);
