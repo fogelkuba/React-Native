7c82f84fc04494984b0f455dcf41a35369928010
fogelkuba <fogelkuba@gmail.com>
2018-01-05 23:09:56 +0100

    app 89

--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import {connext} from 'react-redux'
+import { connect } from 'react-redux'
 
 class LibraryList extends Component {
-
     render(){
         return(
 
         );
     }
 }
+
+const mapStateToProps = state => {
+    return { libraries: state.libraries }
+};
 
 export default connect()(LibraryList);
