2259b1527c66d6affe5ee216b5ade2b533c7c309
fogelkuba <fogelkuba@gmail.com>
2018-01-24 17:09:22 +0100

    added styles config router with add button -132

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
-;
 const RouterComponent = () => {
+
+    const {sceneStyles} = styles;
+
     return(
         <Router sceneStyle={{paddingTop: 60}}>
             <Scene key="root" >
@@ -13,16 +16,29 @@ const RouterComponent = () => {
                 </Scene>
                 <Scene key="main">
                     <Scene
+                        style={sceneStyles}
                         rightTitle="Add"
-                        onRight={ ()=> {console.log('right')}}
+                        onRight={ ()=> Actions.emploeeCreate() }
                         key="employeeList"
                         component={EmployeeList}
                         title="Employees"
+                        initial
                     />
+                    <Scene
+                        style={sceneStyles}
+                        key="emploeeCreate"
+                        component={EmployeeCreate}
+                        title="Create Employee"/>
                 </Scene>
             </Scene>
         </Router>
     )
 };
+
+const styles={
+    sceneStyles:{
+        paddingTop: 5
+    }
+}
 
 export default RouterComponent;
---
+++ manager/src/components/EmployeeCreate.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmploeeCreate extends Component {
+    render(){
+        return(
+            <View>
+                <Text>
+                    Employee Create
+                </Text>
+            </View>
+        );
+    }
+}
+
+export default EmploeeCreate;
